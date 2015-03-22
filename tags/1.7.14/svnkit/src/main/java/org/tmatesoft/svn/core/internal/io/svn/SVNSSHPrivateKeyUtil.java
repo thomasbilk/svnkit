12559d33a7faa4b4b0634d28c180c02ff99bf69c
alex <alex@0a862816-5deb-0310-9199-c792c6ae6c6e>
2015-03-22 20:29:32 +0000

    SVNKit version 1.7.14

---
+++ tags/1.7.14/svnkit/src/main/java/org/tmatesoft/svn/core/internal/io/svn/SVNSSHPrivateKeyUtil.java
@@ -0,0 +1,61 @@
+package org.tmatesoft.svn.core.internal.io.svn;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringWriter;
+
+import org.tmatesoft.svn.core.internal.wc.SVNFileUtil;
+import org.tmatesoft.svn.util.SVNDebugLog;
+import org.tmatesoft.svn.util.SVNLogType;
+
+import com.trilead.ssh2.crypto.PEMDecoder;
+
+public class SVNSSHPrivateKeyUtil {
+
+    public static char[] readPrivateKey(File privateKey) {
+        if (privateKey == null || !privateKey.exists() || !privateKey.isFile() || !privateKey.canRead()) {
+            SVNDebugLog.getDefaultLog().logFine(SVNLogType.NETWORK, "Can not read private key from '" + privateKey + "'");
+            return null;
+        }
+        Reader reader = null;
+        StringWriter buffer = new StringWriter();
+        try {
+            reader = new BufferedReader(new FileReader(privateKey));
+            int ch;
+            while(true) {
+                ch = reader.read();
+                if (ch < 0) {
+                    break;
+                }
+                buffer.write(ch);
+            }
+        } catch (IOException e) {
+            SVNDebugLog.getDefaultLog().logFine(SVNLogType.NETWORK, e);
+            return null;
+        } finally {
+            SVNFileUtil.closeFile(reader);
+        }
+        return buffer.toString().toCharArray();
+    }
+
+    /**
+     * @deprecated
+     */
+    public static boolean isValidPrivateKey(char[] privateKey, String passphrase) {
+        return isValidPrivateKey(privateKey, passphrase != null ? passphrase.toCharArray() : null);
+    }
+    
+    public static boolean isValidPrivateKey(char[] privateKey, char[] passphrase) {
+        try {
+            PEMDecoder.decode(privateKey, passphrase != null ? new String(passphrase) : null);
+        } catch (IOException e) {
+            SVNDebugLog.getDefaultLog().logFine(SVNLogType.NETWORK, e);
+            return false;
+        }        
+        return true;
+    }
+
+}
